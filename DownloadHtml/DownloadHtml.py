fd610558de01f4791c265e21ff5196958bc2a2ec
cl <13546417565@163.com>
2019-06-07 16:10:18 +0800

    using requests to get html using BeautifulSoup to parse html

--- DownloadHtml/DownloadHtml.py
+++ DownloadHtml/DownloadHtml.py
@@ -1,1 +1,16 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+html_url="http://121.42.201.251/se/?switch=7"
+params=[]
+for i in list(range(1,37)):
+    params.append({"sub":i})
+ 
+with open('f:exercise.txt','w',encoding='utf-8') as f:
+    for param in params:
+        response= requests.get(html_url,param)
 
+        soup=BeautifulSoup(response.text,"html.parser")
+        s=soup.find(id="content")
+        f.write(s.text)
+        print("finish download ",response.url)
